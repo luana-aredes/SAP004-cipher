c284eaa08a0d7de1736ce6c299274fc26928c670
Luana Arêdes Costa <luanaredes@hotmail.com>
2020-04-03 14:05:13 -0300

    alteração do js com a parte lógica/finalização.

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,50 @@
 const cipher = {
-  // ...
+
+  encode: (offset, message) => {
+    if (message.length <= 0) {
+      return;
+    }
+
+    let initialMessage = message;
+    let letterOffset = offset;
+    let cipheredMessage = "";
+
+    for (var contador = 0; contador < initialMessage.length; contador++) {
+      let letterPosition = initialMessage.charCodeAt(contador);
+      let letterPositionOffset = (((letterPosition - 65) + letterOffset) % 26) + 65;
+      let letterDecipher = String.fromCharCode(letterPositionOffset);
+      cipheredMessage += letterDecipher;
+    }
+
+    return cipheredMessage;
+  },
+
+  decode: (offsettodecipher, messagetodedecipher) => {
+    if (messagetodedecipher.length <= 0) {
+      return;
+    }
+
+    let messageToDecipher = messagetodedecipher;
+    let letterOffset = offsettodecipher;
+
+    let decipheredMessage = "";
+
+    for (var contador = 0; contador < messageToDecipher.length; contador++) {
+      if ((letterPosition - 65) < letterOffset) {
+        let letterPosition = messageToDecipher.charCodeAt(contador);
+        let letterPositionOffset = (((letterPosition - 65) - letterOffset) + 26) + 65;
+        let letterDecipher = String.fromCharCode(letterPositionOffset);
+      } else {
+        let letterPosition = messageToDecipher.charCodeAt(contador);
+        let letterPositionOffset = (((letterPosition - 65) - letterOffset) % 26) + 65;
+        let letterDecipher = String.fromCharCode(letterPositionOffset);
+      }
+
+      decipheredMessage += letterDecipher;
+    }
+    return decipheredMessage;
+  }
+
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import cipher from './cipher.js';
+import cipher from "./cipher.js";
 
-console.log(cipher);
+let initialMessage = document.getElementById("txtmensage");
+let letterOffset = document.getElementById("offsetnumber");
+
+let buttonCipher = document.getElementById("buttoncipher");
+
+buttonCipher.onclick = () => {
+  let encryptedMessage = cipher.encode(parseInt(letterOffset.value), initialMessage.value.toUpperCase());
+  initialMessage.value = encryptedMessage;
+};
+
+let buttonDecipher = document.getElementById("buttondecipher");
+
+buttonDecipher.onclick = () => {
+  let decipheredMessage = cipher.decode(parseInt(letterOffset.value), initialMessage.value);
+  initialMessage.value = decipheredMessage;
+};
